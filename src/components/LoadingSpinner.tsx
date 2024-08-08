af16767014004ffb7e34b5c821c619328bc62b1c
Kei-dev-1213 <zz2003live@gmail.com>
2024-08-08 21:24:40 +0900

    テスト追加

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -4,6 +4,7 @@ import { FC, memo } from "react";
 export const LoadingSpinner: FC = memo(() => {
   return (
     <Spinner
+      data-testid="loading-spinner"
       thickness="4px"
       speed="0.65s"
       emptyColor="gray.200"
